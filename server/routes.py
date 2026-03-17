c073d33dc36a06a97b0754b2a6531bb6a3bdde68
Simon Perry <github@si-designs.co.uk>
2026-03-17 17:42:00 +0000

    Correct SQL query parameter usage in routes.py
    Fix SQL query execution by passing name directly.

--- server/routes.py
+++ server/routes.py
@@ -14,8 +14,7 @@ def index():
     if name:
         pattern = f"%{name}%"
         cursor.execute(
-            "SELECT * FROM books WHERE name LIKE %s",
-            (pattern,),
+            "SELECT * FROM books WHERE name LIKE %s", name
         )
         books = [Book(*row) for row in cursor]
 
